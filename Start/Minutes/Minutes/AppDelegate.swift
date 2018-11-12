a3af51481b120ca838484d34b80cc868a54bd741
HB <hamsab@Hamsas-MBP-2.guest.corp.microsoft.com>
2018-11-12 11:08:03 -0800

    XcTest

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,4 +1,10 @@
 import UIKit
+//AppSecret be547fd4-1d2f-472c-a6c6-285abfb7cc65
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterPush
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -8,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        MSAppCenter.start("be547fd4-1d2f-472c-a6c6-285abfb7cc65", withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self] )
 
         return true
     }
